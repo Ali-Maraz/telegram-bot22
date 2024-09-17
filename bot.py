db747dd78fb754039c133a1e8e653ce91f3a8b71
Али Мараз <alierik0404@gmail.com>
2024-09-17 23:07:47 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot(TOKEN)
 
 
 # Подключаемся к Google Sheets API
-CREDS_FILE = 'credentials.json'
+CREDS_FILE = 'credential.json'
 creds = Credentials.from_service_account_file(CREDS_FILE, scopes=[
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
